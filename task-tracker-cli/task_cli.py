85e842f7de7d9bec65d25fa09afeec5f07273856
lucky-blessed <blessednkwor7@gmail.com>
2025-12-30 12:56:56 -0700

    Added load and save tasks JSON functionality

---
+++ task-tracker-cli/task_cli.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+"""
+Task Tracker CLI
+A simple command line application to manage tasks.
+"""
+
+import sys
+import json
+import os
+from datetime import datetime
+
+TASKS_FILE = "tasks.json"
+
+def load_tasks():
+    """
+    To load tasks from JSON file.
+    Create an empty one if the file does not exist.
+    Returns a list of tasks.
+    """
+
+    # Checks if the tasks file exists
+    if not os.path.exists(TASKS_FILE):
+        # If it does not exit, create it with an empty list
+        with open(TASKS_FILE, "w") as file:
+            json.dump([], file)
+        return []
+    
+    # If file exists, try to read it
+    try:
+        with open(TASKS_FILE, "r") as file:
+            return json.load(file)
+    except json.JSONDecodeError:
+        # Handles corrupted JSON file
+        print("Error: tasks.json is corrupted.")
+        return []
+    
+def save_tasks(tasks):
+    """
+    Saves the list of tasks to the JSON file.
+    """
+
+    with open(TASKS_FILE, "w") as file:
+        json.dump(tasks, file, indent=2)
+
+
+
+def main():
+    """
+    Entry point of the Task Tracker CLI.
+    Reads command line arguments and routes commands.
+    """
+
+
+    # sys.argv contains all command line arguments 
+    # Example: ['task_cli.py', 'add', 'Buy groceries']
+    args = sys.argv
+
+    # If no command is provided
+    if len(args) < 2:
+        print("Error: No command provided.")
+        print("Usage: python task-cli.py <command> [arguments]")
+        return
+    
+    command = args[1]
+    print(f"Command recieved: {command}")
+
+
+
+if __name__ == "__main__":
+    main()
